96afc6d2676077293b01345c6bc8aec7fd17d1e6
Joonion Bae <joonion@gmail.com>
2022-04-26 17:46:01 +0900

    Unit 26

---
+++ Part.3/Unit.25.LinearRegression.1.R
@@ -0,0 +1,61 @@
+# Unit 25: Linear Regression (1)
+
+windows(width = 7, height = 5)
+
+library(HistData)
+str(GaltonFamilies)
+
+df <- GaltonFamilies
+plot(df$midparentHeight, df$childHeight, 
+     pch = 19, col = adjustcolor("blue", alpha.f = 0.3))
+
+cor(df$midparentHeight, df$childHeight)
+
+model <- lm(childHeight ~ midparentHeight, data = df)
+model
+
+plot(df$midparentHeight, df$childHeight, 
+     pch = 19, col = adjustcolor("blue", alpha.f = 0.3))
+abline(model, col = "tomato", lty = 1, lwd = 3)
+
+str(df)
+
+color.m <- adjustcolor("steelblue", alpha.f = 0.3)
+color.f <- adjustcolor("orange", alpha.f = 0.3)
+
+with(df, 
+     plot(midparentHeight, childHeight, pch = 19, 
+          col = ifelse(gender == "male", color.m, color.f)))
+
+model.m <- lm(childHeight ~ midparentHeight, 
+              data = subset(df, gender == "male"))
+abline(model.m, col = "blue", lty = 1, lwd = 3)
+
+model.f <- lm(childHeight ~ midparentHeight, 
+              data = subset(df, gender == "female"))
+abline(model.f, col = "tomato", lty = 1, lwd = 3)
+
+set.seed(14)
+x <- runif(n = 7, min = 0, max = 10)
+y <- 3 + 2 * x + rnorm(n = 7, mean = 0, sd = 5)
+round(x, 2)
+round(y, 2)
+
+model <- lm(y ~ x, data = df)
+coef(model)
+intercept <- coef(model)[1]
+slope <- coef(model)[2]
+y.hat <- intercept + slope * x
+round(y.hat, 2)
+r <- y - y.hat
+round(r, 2)
+
+plot(x, y, pch = 19, col = "steelblue", xlim = c(2, 10), ylim = c(5, 30))
+abline(model, lwd = 2, col = "steelblue")
+abline(a = intercept + 5, b = slope, lty = 2, lwd = 2, col = "orange")
+abline(a = intercept - 5, b = slope, lty = 2, lwd = 2, col = "orange")
+abline(a = intercept, b = slope + 1, lty = 3, lwd = 2, col = "violet")
+abline(a = intercept, b = slope - 1, lty = 3, lwd = 2, col = "violet")
+legend(x = 2, y = 30, lwd = 2, col = "steelblue",
+       legend = paste("y =", intercept, "+", slope, "* x"))
+
---
+++ Part.3/Unit.26.LinearRegression.2.R
@@ -0,0 +1,63 @@
+# Unit 26: Linear Regression (2)
+
+windows(width = 7, height = 5)
+
+?lm
+
+# install.packages(car)
+library(car)
+str(Prestige)
+
+?Prestige
+df <- Prestige
+plot(income ~ education, data = df, pch = 19, col = "steelblue")
+
+cor(df$education, df$income)
+
+formula <- income ~ education
+lm(formula = formula, data = Prestige)
+
+model <- lm(formula = formula, data = Prestige)
+abline(model, lwd = 2, col = "tomato")
+
+summary(model)
+
+mod.summ <- summary(model)
+coef(model.summ)
+
+coef(model)
+summary(resid(model))
+confint(model)
+anova(model)
+
+library(car)
+str(Prestige)
+df <- subset(Prestige, select = c(2, 1, 3, 4))
+str(df)
+cor(df)
+
+plot(df, pch = 19, col = "steelblue")
+
+formula = income ~ education + women + prestige
+lm(formula, data = df)
+
+model <- lm(income ~ ., data = df)
+summary(model)
+
+library(stargazer)
+stargazer(model, type="text", no.space = TRUE)
+
+library(car)
+formula <- income ~ education + I(education^2)
+lm(formula, data = Prestige)
+
+model <- lm(formula, data = Prestige)
+summary(model)
+
+plot(income ~ education, data = Prestige, pch = 19, col = "steelblue")
+library(dplyr)
+with(Prestige,
+     lines(arrange(data.frame(education, fitted(model)), education), 
+           lty = 1, lwd = 3, col = "tomato"))
+
+?arrange
